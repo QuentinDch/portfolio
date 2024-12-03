86e37ad870757ffbc6e68d1174bcf8477e936646
Quentin <quentin.caflo@gmail.com>
2024-12-03 17:13:54 +0100

    fix(footer): remove hardcoded year from HTML and rely on dynamic JavaScript

--- src/js/components/dateUtils.js
+++ src/js/components/dateUtils.js
@@ -1,7 +1,7 @@
 // dateUtils.js
 
 export function initializeFooterDate() {
-  const yearElement = document.querySelector("#year");
+  const yearElement = document.querySelector("#year small");
   if (yearElement) {
     yearElement.textContent = new Date().getFullYear();
   }
