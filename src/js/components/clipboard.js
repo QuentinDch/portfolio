a313170bff053eafe5dd0b715f5442ec83cd2aa9
Quentin <quentin.caflo@gmail.com>
2025-01-17 11:06:08 +0100

    fix(contact): fix clipboard copy animation

--- src/js/components/clipboard.js
+++ src/js/components/clipboard.js
@@ -10,14 +10,14 @@ export function clipboard() {
         .writeText(email)
         .then(() => {
           copyEmail.classList.add("clicked");
+
+          setTimeout(() => {
+            copyEmail.classList.remove("clicked");
+          }, 3000);
         })
         .catch((err) => {
           console.error("Error copying to clipboard:", err);
         });
-    });
-
-    copyEmail.addEventListener("mouseleave", () => {
-      copyEmail.classList.remove("clicked");
     });
   }
 }
